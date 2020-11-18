b6502b73e825a2bc4de101374acd94334aefd6a8
Paulina Kluvancova <kluvancova@onlima.sk>
2020-11-18 14:20:23 +0100

    registration fragment

--- app/src/main/java/com/example/tuktuk/RegistrationFragment.kt
+++ app/src/main/java/com/example/tuktuk/RegistrationFragment.kt
@@ -1,15 +1,16 @@
 package com.example.tuktuk
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.tuktuk.databinding.FragmentLoginBinding
+import com.example.tuktuk.databinding.FragmentRegistrationBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +18,28 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RegistrationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
-    }
+        val binding = DataBindingUtil.inflate<FragmentRegistrationBinding>(inflater,
+            R.layout.fragment_registration,container,false)
+        // Set the onClickListener for the submitButton
+        binding.button3.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER")
+        { view: View ->
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegistrationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistrationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+            val name = binding.editTextTextPersonName.text;
+            val password = binding.editTextTextPassword2.text;
+            val passwordCheck = binding.editTextTextPassword3.text;
+            val birth = binding.editTextNumber.text;
+            Log.i("INFO", name.toString())
+            Log.i("INFO", password.toString())
+            Log.i("INFO", passwordCheck.toString())
+            Log.i("INFO", birth.toString())
+
+        }
+        return binding.root
     }
 }
