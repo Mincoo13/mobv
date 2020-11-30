10f9e673e5a719a869d659d3f7ca831183b1528c
lea <lealapsanska@gmail.com>
2020-11-30 23:40:31 +0100

    HomeFragment - RecyclerView, ProfileFragment

--- app/src/main/java/com/example/tuktuk/HomeFragment.kt
+++ app/src/main/java/com/example/tuktuk/HomeFragment.kt
@@ -6,9 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tuktuk.Adapter.MediaAdapter
 import com.example.tuktuk.databinding.FragmentHomeBinding
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,6 +23,31 @@ class HomeFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
             R.layout.fragment_home,container,false)
 
+        binding.toProfile.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
+        }
+
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val exampleList = generateDummyList(20)
+        recycler_view.adapter = MediaAdapter(exampleList)
+        recycler_view.layoutManager = LinearLayoutManager(this.requireContext())
+        recycler_view.setHasFixedSize(true)
+
+    }
+
+    private fun generateDummyList(size: Int): List<MediaObject> {
+        val list = ArrayList<MediaObject>()
+        for (i in 0 until size) {
+            val item = MediaObject( "Item $i", "Line 2")
+            list += item
+        }
+        return list
+    }
+
 }
--- app/src/main/java/com/example/tuktuk/ProfileFragment.kt
+++ app/src/main/java/com/example/tuktuk/ProfileFragment.kt
@@ -1,7 +1,23 @@
 package com.example.tuktuk
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.tuktuk.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
+        val binding = DataBindingUtil.inflate<FragmentProfileBinding>(inflater,
+            R.layout.fragment_profile,container,false)
+
+        return binding.root
+    }
 }
