b91207d7b672d6fd5a90d2f5a7706b1d097ef6d3
xlacko <lacko.k27@gmail.com>
2020-12-06 18:16:42 +0100

    sharing a placeholder text from each post

--- app/src/main/java/com/example/tuktuk/Adapter/VideoGridAdapter.kt
+++ app/src/main/java/com/example/tuktuk/Adapter/VideoGridAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.tuktuk.Adapter
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -11,9 +13,11 @@ import com.example.tuktuk.databinding.MediaObjectBinding
 import com.example.tuktuk.network.responses.VideosResponse
 import kotlinx.android.synthetic.main.media_object.view.*
 
-class VideoGridAdapter() : ListAdapter<VideosResponse, VideoGridAdapter.VideoViewHolder>(DiffCallback) {
-
+class VideoGridAdapter() : ListAdapter<VideosResponse, VideoGridAdapter.VideoViewHolder>(
+    DiffCallback
+) {
     class VideoViewHolder(private var binding: MediaObjectBinding): RecyclerView.ViewHolder(binding.root) {
+        val shareBtn: ImageView = itemView.shareBtn
         fun bind(video: VideosResponse) {
             binding.video = video
             binding.executePendingBindings()
@@ -27,6 +31,11 @@ class VideoGridAdapter() : ListAdapter<VideosResponse, VideoGridAdapter.VideoVie
     override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
         val video = getItem(position)
         holder.bind(video)
+        holder.shareBtn.setOnClickListener() {
+            val context = holder.itemView.context
+            val urlVideo = "Kukaj na toto super video cislo " + position.toString() + " zo skvelej aplikacie TukTuk!"
+            context?.startActivity(shareVideo(urlVideo))
+        }
     }
 
     companion object DiffCallback : DiffUtil.ItemCallback<VideosResponse>() {
@@ -39,4 +48,11 @@ class VideoGridAdapter() : ListAdapter<VideosResponse, VideoGridAdapter.VideoVie
         }
     }
 
+    private fun shareVideo(urlVideo: String) : Intent {
+        return Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, urlVideo)
+            type = "text/plain"
+        }
+    }
 }
